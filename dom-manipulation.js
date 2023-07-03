ee4fcb532e6f7b1aea6b4f637355cc49449f1d52
rdwanmuhamad <mrdwn624@gmail.com>
2023-07-03 21:30:56 +0800

    DOM Manipulation

---
+++ dom-manipulation.js
@@ -0,0 +1,23 @@
+// innerHTML
+const judul = document.getElementById("judul");
+// judul.innerHTML = "<h5>Hello, Muhamad Ridwan</h5>";
+
+// const sectionA = document.querySelector("section#a");
+// sectionA.innerHTML = "<p>Hello, World!</p>";
+
+// element.style.<propetiCSS>
+// judul.style.color = "lightblue";
+// judul.style.backgroundColor = "salmon";
+
+// elementGetAttribute
+// judul.setAttribute("name", "title");
+// const a = document.querySelector("a");
+// a.setAttribute('id', 'link')
+
+// element.classList
+judul.classList.add("label", "test", "test2");
+// judul.classList.remove("label");
+// judul.classList.toggle("label");
+// judul.classList.item(2)
+// judul.classList.contains(2)
+judul.classList.replace('test', 'test1')
