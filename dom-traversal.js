5df237a590e5c1eec308d96b5e6182754f3ddb09
rdwanmuhamad <mrdwn624@gmail.com>
2023-07-05 14:42:13 +0800

    dom traversal

---
+++ dom-traversal.js
@@ -0,0 +1,24 @@
+// const close = document.querySelector(".close");
+// const card = document.querySelector(".card");
+
+// close.addEventListener("click", function () {
+//   card.style.display = "none";
+// });
+
+// DOM Traversal
+const close = document.querySelectorAll(".close");
+// const card = document.querySelectorAll(".card");
+
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener("click", function (e) {
+//     // card[i].style.display = "none";
+//     // close[i].parentElement.style.display = 'none'
+//     e.target.parentElement.style.display = 'none'
+//   });
+// }
+
+close.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    e.target.parentElement.style.display = "none";
+  });
+});
