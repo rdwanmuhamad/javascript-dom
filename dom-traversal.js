26ae9acd1fe32ff83ae6b46ef712fda03632c2fa
rdwanmuhamad <mrdwn624@gmail.com>
2023-07-05 15:43:36 +0800

    event bubling

--- dom-traversal.js
+++ dom-traversal.js
@@ -6,7 +6,7 @@
 // });
 
 // DOM Traversal
-const close = document.querySelectorAll(".close");
+// const close = document.querySelectorAll(".close");
 // const card = document.querySelectorAll(".card");
 
 // for (let i = 0; i < close.length; i++) {
@@ -17,9 +17,27 @@ const close = document.querySelectorAll(".close");
 //   });
 // }
 
-close.forEach(function (el) {
-  el.addEventListener("click", function (e) {
+// close.forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.target.parentElement.style.display = "none";
+//     e.preventDefault();
+//     e.stopPropagation()
+//   });
+// });
+
+// const cards = document.querySelectorAll(".card");
+// cards.forEach(function (card) {
+//   card.addEventListener('click', function(e){
+//     alert('oke')
+//   })
+// })
+
+// event bubbling
+const container = document.querySelector(".container");
+
+container.addEventListener("click", function (e) {
+  // console.log(e.target);
+  if (e.target.className == "close") {
     e.target.parentElement.style.display = "none";
-    e.preventDefault();
-  });
+  }
 });
